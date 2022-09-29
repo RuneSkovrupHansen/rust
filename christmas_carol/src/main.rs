544b2c8db37f2666e0f78faa59710b03f1b868ce
RuneSkovrupHansen <Runeskovrup@gmail.com>
2022-09-29 16:08:26 +0200

    Add christmas_carol

---
+++ christmas_carol/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let verses: [&str; 12] = [
+        "a partridge in a pear tree",
+        "Two turtle doves",
+        "Three French hens",
+        "Four calling birds",
+        "Five golden rings",
+        "Six geese a-laying",
+        "Seven swans a-swimming",
+        "Eight maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+
+    for i in 0..verses.len() {
+        println!("On the {i}. day of Christmas,\nmy true love sent to me");
+        for j in (0..=i).rev() {
+            let verse = if i != 0 && j == 0 {
+                format!("And {}", verses[j])
+            } else {
+                capitalize_first_letter(verses[j])
+            };
+
+            let punctuation = if j == 0 { "." } else { "," };
+            println!("{verse}{punctuation}");
+        }
+        println!()
+    }
+}
+
+fn capitalize_first_letter(s: &str) -> String {
+    s[0..1].to_uppercase() + &s[1..]
+}
