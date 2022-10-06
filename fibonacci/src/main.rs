7a3676e866124ca856106b261a5680b6faa26707
RuneSkovrupHansen <runeskovrup@gmail.com>
2022-10-06 18:32:24 +0200

    Fix fibonacci

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,17 +1,23 @@
 fn main() {
-    let fib: u32 = get_fibonacci_number(4);
-
+    let fib: u32 = get_fibonacci_number(3);
     println!("The fibonacci number is: {fib}");
 }
 
 fn get_fibonacci_number(number: u32) -> u32 {
-    let mut value: u32 = 0;
-    let mut temp: u32 = 1;
+    return match number {
+        0..=1 => number,
+        _ => {
+            let mut n1: u32 = 0;
+            let mut n2: u32 = 1;
+            let mut value: u32 = 0;
 
-    for _ in 0..number - 1 {
-        value += temp;
-        temp = value;
-    }
+            for _ in 0..=(number - 2) {
+                value = n2 + n1;
+                n1 = n2;
+                n2 = value;
+            }
 
-    return value;
+            value
+        }
+    };
 }
